cf41d3277a2eda75442bc7cd4dccee0e624f1a75
alexkonkin <alexkonkin@gmail.com>
2019-10-21 17:16:36 +0300

    Update test_mysql.sh

--- app/test_mysql.sh
+++ app/test_mysql.sh
@@ -3,10 +3,14 @@
 
 set -e
 
-until /usr/bin/mysql -u root -h db --password=test -e 'use test; select count(*) from users;'; do
+DB_USER=$1
+DB_PASSWORD=$2
+CMD=$3
+
+until /usr/bin/mysql -u $DB_USER -h db --password=$DB_PASSWORD -e 'use test; select count(*) from users;'; do
   >&2 echo "Mysql is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Mysql is up - executing command"
-exit 0
+>&2 echo "Mysql is up - we can continue"
+${CMD}
